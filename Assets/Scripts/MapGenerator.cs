fe2190e4a0dfeb4f58d9481ef6d363aebb0d94f8
Adrien Leroy <leroy.adrien.76@gmail.com>
2020-10-06 22:42:11 +0200

    Jeu_Saint_Aubin

---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class MapGenerator
+{
+    public static int[,] GeneratePoints(int height, int width)
+    {
+        int[,] matrix = new int[height, width];
+        for (int y = 0; y < height; y++)
+            for (int x = 0; x < width; x++)
+            {
+                matrix[y, x] = Random.Range(0, 2);
+            }
+        return matrix;
+    }
+/*
+    public static List<Vector3[]> GenerateLines(int[,] pointMatrix)
+    {
+
+    }
+    */
+}
